be5cb112408eebee353baedc70d9f0ea326e5510
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-04-19 18:03:24 -0300

    feat: criando componentes base de usuario e banco de dados

---
+++ src/app/schemas/usuario.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, SchemaFactory } from "@nestjs/mongoose";
+import { IUsuario } from "../models/usuario.interface";
+
+export class UsuarioSchemaDB implements IUsuario {
+    @Prop({ required: true })
+    nome: string;
+    
+    @Prop({ required: true })
+    email: string;
+    
+    @Prop({ required: true })
+    senha: string;
+    
+    @Prop({ required: true })
+    perfil: string;
+}
+
+export const UsuarioSchema = SchemaFactory.createForClass(UsuarioSchemaDB);
+
+UsuarioSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        ret.id = ret._id;
+        
+        delete ret._id;
+        delete ret.__v;
+        delete ret.senha;
+    },
+})
---
+++ src/database/database.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { ModelDefinition, MongooseModule } from "@nestjs/mongoose";
+import { Env } from "src/utils/env";
+
+@Module({})
+export class DataBaseModule{
+    static async forRoot(){
+        return MongooseModule.forRoot(Env.database.stringConnection, {
+            dbName: Env.database.dbName,
+        })
+    }
+
+    static async forFeature(models?: ModelDefinition[]){
+        return MongooseModule.forFeature(models);
+    }
+}
