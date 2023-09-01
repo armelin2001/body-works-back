cf169384b666b45b25e7cb54be6b79f8b5810314
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-08-31 21:19:05 -0300

    fix: trocando tipagem de paramtetro da controller

--- src/app/modules/usuario/usuario.controller.ts
+++ src/app/modules/usuario/usuario.controller.ts
@@ -3,6 +3,7 @@ import { UsuarioService } from "./usuario.service";
 import { LoginDTO, UsuarioDTO } from "./dto/usario.dto";
 import { IUsuario } from "src/app/modules/usuario/entity/usuario.interface";
 import { IUsuarioAcademia } from "../usuario-academia/entity/usuario-academia.interface";
+import { StatusPagamento } from "src/utils/constants/status-pagamento";
 
 @Controller("usuario")
 export class UsuarioController {
@@ -33,8 +34,14 @@ export class UsuarioController {
     async obterTodos(): Promise<{ dados: IUsuario[]; quantidade: number }> {
         return await this.usuarioService.obterTodos();
     }
-    @Patch(':id/status-pagamento')
-    async atualizarStatusPagamento(@Param('id') id: string, @Body('statusPagamento') statusPagamento: string): Promise<IUsuario> {
-        return await this.usuarioService.atualizarStatusPagamento(id, statusPagamento);
+    @Patch(":id/status-pagamento")
+    async atualizarStatusPagamento(
+        @Param("id") id: string,
+        @Body("statusPagamento") statusPagamento: StatusPagamento,
+    ): Promise<IUsuario> {
+        return await this.usuarioService.atualizarStatusPagamento(
+            id,
+            statusPagamento,
+        );
     }
 }
