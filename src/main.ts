9283a0f56a7ba151409e9be08b3bec75fdda848f
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-11-04 20:55:24 -0300

    chore: arrumando porta

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule); //, { cors: true }
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT || 3010);
 }
 bootstrap();
