640da996e93a73e24f2e85b97e259ddf06528c0d
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-11-05 23:35:18 -0300

    chore: adicionando rota swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import "./utils/load-env";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+    const config = new DocumentBuilder()
+        .setTitle("Body works")
+        .setDescription("Api do projeto body works")
+        .setVersion("1.0")
+        .addTag("body works")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
     await app.listen(process.env.PORT || 3010);
 }
 bootstrap();
